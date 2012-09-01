da871304259393dd4faae62f0f326d0968c61595
jeremyclark <jeremyb.clark@gmail.com>
2012-09-01 14:23:36 -0500

    lots of changes.. recfactored game/controls

--- script.js
+++ script.js
@@ -17,10 +17,7 @@ $(document).ready(function(){
   canvas.width = width;
   canvas.height = height;
 
-  var player,
-      ambulanceCount = 1,
-      ambulances = [],
-      position = 0,
+  var position = 0,
       gravity = 0.2,
       animloop,
       flag = 0,
@@ -30,170 +27,195 @@ $(document).ready(function(){
       score = 0,
       firstRun = true;
 
-  var Game = function() {
-    this.tick = function() {
-      ctx.clearRect(0, 0, width, height);
+	  
+	var Player = function() {
+		this.vy = 0;
+		this.vx = 0;
 
-      player.y += player.vy;
-      //player.vy += gravity;
+		this.isMovingLeft = false;
+		this.isMovingRight = false;
+		this.isDead = false;
+		this.isFiring = false;
+		this.direction = 'left';
 
-      if (player.vy > 0 &&
-        (player.x + 15 < 260) &&
-        (player.x + player.width - 15 > 155) &&
-        (player.y + player.height > 475) &&
-        (player.y + player.height < 500))
-        player.jump();
+		this.width = 99;
+		this.height = 134;
 
-      if (dir == "left") {
-        player.dir = "left";
-        if (player.vy < -7 && player.vy > -15) player.dir = "left_land";
-      } else if (dir == "right") {
-        player.dir = "right";
-        if (player.vy < -7 && player.vy > -15) player.dir = "right_land";
-      }
+		//Sprite clipping
+		this.cx = 0;
+		this.cy = 0;
+		this.cwidth = 99;
+		this.cheight = 134;
 
-      //Adding keyboard controls
-      document.onkeydown = function(e) {
-        var key = e.keyCode;
+		this.x = width / 2 - this.width / 2;
+		this.y = 10;
 
-        if (key == 37) {
-            dir = "left";
-            player.isMovingLeft = true;
-          } else if (key == 39) {
-            dir = "right";
-            player.isMovingRight = true;
-          }else if (key == 32) {
-            player.isFiring = true;
-          }
+		this.move = function() {
+			this.y +=this.vy;
+		  //player.vy += gravity;
 
-        };
+		  if (this.vy > 0 &&
+			(this.x + 15 < 260) &&
+			(this.x +this.width - 15 > 155) &&
+			(this.y +this.height > 475) &&
+			(this.y +this.height < 500))
+			this.jump();
+		  
 
-        document.onkeyup = function(e) {
-          var key = e.keyCode;
+			//Accelerations produces when the user hold the keys
+			if (this.isMovingLeft === true) {
+			  this.x += this.vx;
+			  this.vx -= 0.15;
+			} else {
+			  this.x += this.vx;
+			  if (this.vx < 0) this.vx += 0.1;
+			}
 
-          if (key == 37) {
-            dir = "left";
-            player.isMovingLeft = false;
-          } else if (key == 39) {
-            dir = "right";
-            player.isMovingRight = false;
-          }else if (key == 32) {
-            player.isFiring = false;
-          }
-        };
+			if (this.isMovingRight === true) {
+			  this.x += this.vx;
+			  this.vx += 0.15;
+			} else {
+			  this.x += this.vx;
+			  if (this.vx > 0) this.vx -= 0.1;
+			}
 
-        //Accelerations produces when the user hold the keys
-        if (player.isMovingLeft === true) {
-          player.x += player.vx;
-          player.vx -= 0.15;
-        } else {
-          player.x += player.vx;
-          if (player.vx < 0) player.vx += 0.1;
-        }
+			//Make the this move through walls
+			if (this.x > width) this.x = 0 - this.width;
+			else if (this.x < 0 - this.width) this.x = width;
+			
+			this.draw();
+		};
+		
+		
+		//Function to draw it
+		this.draw = function() {
+		  try {
+			if (this.direction == 'right') this.cx = 99;
+			else if (this.direction == 'left') this.cx = 0;
 
-        if (player.isMovingRight === true) {
-          player.x += player.vx;
-          player.vx += 0.15;
-        } else {
-          player.x += player.vx;
-          if (player.vx > 0) player.vx -= 0.1;
-        }
+			if(this.isFiring) this.cy = 134;
+			else this.cy = 0;
 
-        //Make the player move through walls
-        if (player.x > width) player.x = 0 - player.width;
-        else if (player.x < 0 - player.width) player.x = width;
+			ctx.drawImage(image, this.cx, this.cy, this.cwidth, this.cheight, this.x, this.y, this.width, this.height);
+		  } catch (e) {}
+		};
 
-        player.draw();
-        for(var i=0;i<ambulanceCount;i++){
-          ambulances[i].draw();
-        }
-      }
-    this.reset = function() {
-      this.window.reload;
-    }
-  };
-  var game = new Game();
+		this.jump = function() {
+		  this.vy = 0;
+		};
 
-  var Player = function() {
-    this.vy = 0;
-    this.vx = 0;
+		this.jumpHigh = function() {
+		  this.vy = 0;
+		};
+	};
 
-    this.isMovingLeft = false;
-    this.isMovingRight = false;
-    this.isDead = false;
-    this.isFiring = false;
+	var Ambulance = function() {
+		this.width = 107;
+		this.height = 77;
+		this.speed = 5;
+		this.isDead = false;
 
-    this.width = 99;
-    this.height = 134;
+		this.x = 850;
+		this.y = 600 - this.height;
+		this.cx = 47;
+		this.cy = 272;
+		this.cwidth = 107;
+		this.cheight = 74;
+		
+		this.move = function() {		
+			if(this.isDead) return;
+			
+			this.x -= this.speed;
+			this.draw();
+		};
+		
+		this.draw = function() {
+		  try{
+			ctx.drawImage(image, this.cx, this.cy, this.cwidth, this.cheight, this.x, this.y, this.width, this.height);
+		  }catch(e){}
+		};
+	};	  
+	  
+	  
+	var Game = function() {
+		var self = this;
+		this.player = null;
+		this.ambulances = [];
+		this.ambulanceCount = 1;
+		
+		
+		this.init = function() {
+			this.player = new Player();
+			this.addAmbulance(this.ambulanceCount);
+		};
+		
+		this.tick = function() {
+		  ctx.clearRect(0, 0, width, height);
+		  this.player.move();
+		  
+		  for(var i=0; i < this.ambulanceCount; i++){
+			this.ambulances[i].move();
+		  }
+			
+		  this.deleteAmbulances();
+		}
+		  
+		this.reset = function() {
+		  this.window.reload;
+		}
+		
+		this.addAmbulance = function(count) {
+			for(var i=0; i < count; i++) {
+				this.ambulances.push(new Ambulance());
+			}
+		};
+		
+		this.deleteAmbulances = function() {
+			for(var i = 0; i < this.ambulances.length; i++) {
+				if(this.ambulances[i].x < (-1 * this.ambulances[i].width)) {
+					this.ambulances[i].isDead = true;
+				}
+			}
+		};
+		
+		//Adding keyboard controls
+		document.onkeydown = function(e) {
+			var key = e.keyCode;
 
-    //Sprite clipping
-    this.cx = 0;
-    this.cy = 0;
-    this.cwidth = 99;
-    this.cheight = 134;
+			if (key == 37) {
+				self.player.direction = 'left';
+				self.player.isMovingLeft = true;
+			} else if (key == 39) {
+				self.player.direction = 'right';
+				self.player.isMovingRight = true;
+			}else if (key == 32) {
+				self.player.isFiring = true;
+			 }
+		};
 
-    this.dir = "left";
+		document.onkeyup = function(e) {
+			var key = e.keyCode;
 
-    this.x = width / 2 - this.width / 2;
-    this.y = 10;
+			if (key == 37) {
+				self.player.direction = 'left';
+				self.player.isMovingLeft = false;
+			} else if (key == 39) {
+				self.player.direction = 'right';
+				self.player.isMovingRight = false;
+			}else if (key == 32) {
+				self.player.isFiring = false;
+			}
+		};		
+	 };
+ 
+	
+	var game = new Game();
+	game.init();
+	
+	menuLoop = function() {
+		game.tick();
+		requestAnimFrame(menuLoop);
+	 };
 
-    //Function to draw it
-    this.draw = function() {
-      try {
-        if (this.dir == "right") this.cx = 99;
-        else if (this.dir == "left") this.cx = 0;
-
-        if(this.isFiring) this.cy = 134;
-        else this.cy = 0;
-
-        ctx.drawImage(image, this.cx, this.cy, this.cwidth, this.cheight, this.x, this.y, this.width, this.height);
-      } catch (e) {}
-    };
-
-    this.jump = function() {
-      this.vy = 0;
-    };
-
-    this.jumpHigh = function() {
-      this.vy = 0;
-    };
-  };
-
-  player = new Player();
-
-  var Ambulance = function() {
-    this.width = 107;
-    this.height = 77;
-
-    this.x = 850;
-    this.y = 600 - this.height;
-    console.log(window.innerWidth);
-    this.cx = 47;
-    this.cy = 272;
-    this.cwidth = 107;
-    this.cheight = 74;
-
-    this.draw = function() {
-      try{
-        ctx.drawImage(image, this.cx, this.cy, this.cwidth, this.cheight, this.x, this.y, this.width, this.height);
-      }catch(e){}
-    };
-
-  };
-
-  for(var i=0; i<ambulanceCount;i++) {
-    ambulances.push(new Ambulance());
-  }
-
-
-  menuLoop = function() {
-    game.tick();
-    requestAnimFrame(menuLoop);
-  };
-
-  menuLoop();
-
-  $('#play').click(function(){
-    init();
-  });
+	menuLoop();
 });
